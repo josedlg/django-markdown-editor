4f59f31074d42990f3da1fe26d4760ecc7a7de90
Jose Delaguarda <josedelaguarda@outlook.com>
2021-07-27 16:26:47 -0400

    added \n  fix

--- martor_demo/app/testPosts.py
+++ martor_demo/app/testPosts.py
@@ -9,4 +9,5 @@ class PostTestCase(TestCase):
         post = Post(title="My Title", description="Blurb", wiki="Post Body")
         self.assertEqual(post.title, "My Title")
         self.assertEqual(post.description, "Blurb")
-        self.assertEqual(post.wiki, "Post Body") \n
+        self.assertEqual(post.wiki, "Post Body")
+        \n
